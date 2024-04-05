bf6c4682029a6aa624aa5fb909200958170fd035
David Villanueva <david.villanueva.dev@gmail.com>
2024-04-04 19:06:41 -0500

    feat(challenge-2): BF async resolution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,28 @@
 "use strict";
 
 // Print all entries, across all of the *async* sources, in chronological order.
+const MinHeap = require("./min-heap");
+module.exports = async (logSources, printer) => {
+/**
+ * Brute force approach, not considering network latency for logSources. That could affect the optimization of the network usage.
+ */
+    const logsQueue = new MinHeap();
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+    for (var index = 0; index < logSources.length; index++) {    
+      const currLog = await logSources[index].popAsync();
+      const item = { value: currLog.date, sourceIdx: index, log: currLog };
+      logsQueue.insert(item);
+    }
+
+    while(!logsQueue.isEmpty()){
+      let { log, sourceIdx } = logsQueue.extractMin();
+      printer.print(log);
+
+      const currLog = await logSources[sourceIdx].popAsync();
+      if (currLog) {
+        logsQueue.insert({ value: currLog.date, sourceIdx, log: currLog });
+      }
+    }
+    printer.done()
+    console.log("Async sort complete.");
 };
