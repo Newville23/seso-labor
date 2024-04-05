9d90835d2f9a6c9a69e2e9a47a83efbda8e15869
David Villanueva <david.villanueva.dev@gmail.com>
2024-04-04 19:28:58 -0500

    feat(challenge-2): optimize async resolution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,26 +3,30 @@
 // Print all entries, across all of the *async* sources, in chronological order.
 const MinHeap = require("./min-heap");
 module.exports = async (logSources, printer) => {
-/**
- * Brute force approach, not considering network latency for logSources. That could affect the optimization of the network usage.
- */
-    const logsQueue = new MinHeap();
+  const logsQueue = new MinHeap();
 
-    for (var index = 0; index < logSources.length; index++) {    
-      const currLog = await logSources[index].popAsync();
-      const item = { value: currLog.date, sourceIdx: index, log: currLog };
-      logsQueue.insert(item);
+  //create an array of promises to avoid a bottelneck blocking request over logSources
+  const promiseArr = logSources.map((logSource) => logSource.popAsync());
+
+  // once we have a array of promises we could extract the logs data
+  const logsData = await Promise.all(promiseArr);
+
+  // insert the logs to the queue
+  logsData.forEach((log, idx) => {
+    if (log) {
+      logsQueue.insert({ value: log.date, sourceIdx: idx, log });
     }
+  });
 
-    while(!logsQueue.isEmpty()){
-      let { log, sourceIdx } = logsQueue.extractMin();
-      printer.print(log);
+  while (!logsQueue.isEmpty()) {
+    let { log, sourceIdx } = logsQueue.extractMin();
+    printer.print(log);
 
-      const currLog = await logSources[sourceIdx].popAsync();
-      if (currLog) {
-        logsQueue.insert({ value: currLog.date, sourceIdx, log: currLog });
-      }
+    const currLog = await logSources[sourceIdx].popAsync();
+    if (currLog) {
+      logsQueue.insert({ value: currLog.date, sourceIdx, log: currLog });
     }
-    printer.done()
-    console.log("Async sort complete.");
+  }
+  printer.done();
+  console.log("Async sort complete.");
 };
